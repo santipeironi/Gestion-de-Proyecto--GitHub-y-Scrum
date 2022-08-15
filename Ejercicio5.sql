a85b58f14c70a8d7d0391b1635576f11b40613ad
Santiago Peironi <106498214+santipeironi@users.noreply.github.com>
2022-08-14 21:46:55 -0300

    Ejercicio5 MySql

---
+++ Ejercicio5.sql
@@ -0,0 +1,8 @@
+INSERT INTO dueno(DNI, Nombre, Apellido, Telefono, Direccion) 
+values (8912312,"Pedro","Gonzalez",35150123,"Lima 324");
+
+INSERT INTO perro(ID_Perro, Nombre, Fecha_nac, Sexo, DNI_dueno) 
+values (23,"Tomy","22/04/18","M",8912312);
+
+INSERT INTO perro(ID_Perro, Nombre, Fecha_nac, Sexo, DNI_dueno) 
+values (37,"Luna","03/07/20","F",8912312);
